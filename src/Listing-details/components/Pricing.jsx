b026325dd22f3d082cbe80bd51d0b18209ff266b
Developer9985 <sudeepdasdas17815@gmail.com>
2024-11-21 11:43:10 +0530

    change to rupee

--- src/Listing-details/components/Pricing.jsx
+++ src/Listing-details/components/Pricing.jsx
@@ -6,7 +6,7 @@ function Pricing({carDetail}) {
   return (
     <div className='bg-[#dfecf594] rounded-3xl absolute w-[500px] h-[300px] shadow-lg'>
         <h2 className='font-bold text-4xl p-3 mx-14 mt-10'>Our Price</h2>
-       <h2 className='font-bold text-3xl mx-[70px] mt-3'>${carDetail?.sellingPrice}</h2>
+       <h2 className='font-bold text-3xl mx-[70px] mt-3'>₹{carDetail?.sellingPrice}</h2>
 
        <Button className='bg-[#7288f3] text-white p-6 w-[300px] mx-[100px] my-[60px]'><FaTag /> Make an Offer Price</Button>
     </div>
